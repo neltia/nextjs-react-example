07d52c4f6259c64e8a71bbb0e4201ce675f2b33e
Hyunseo <dsz08082@naver.com>
2025-06-14 23:45:25 +0900

    rename: 04_react_tsx -> 04_react_mini_projects

--- 04_react_tsx/01_tsx_basic/src/components/ConditionalRender.tsx
+++
@@ -1,9 +0,0 @@
-type Props = { isLoggedIn: boolean };
-
-export default function ConditionalRender({ isLoggedIn }: Props) {
-  return (
-    <div>
-      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
-    </div>
-  );
-}
--- 04_react_tsx/01_tsx_basic/src/components/GreetingWithProps.tsx
+++
@@ -1,9 +0,0 @@
-type GreetingProps = { name: string };
-
-function Greeting({ name }: GreetingProps) {
-  return <p>Hello, {name}!</p>;
-}
-
-export default function GreetingWithProps() {
-  return <Greeting name="Mocha" />;
-}
